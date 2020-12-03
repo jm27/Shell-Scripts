c09d2ed2e99b8d6f47b5ba5f7a19e4a5454121cb
Jesus Esquer <jme2791@gmail.com>
2020-12-02 18:06:22 -0700

    Multiplication and addition working

--- Function_Demo.sh
+++ Function_Demo.sh
@@ -3,6 +3,21 @@
 # Author : Jesus Esquer
 # Script Name : Function Demo
 
+addition()
+{
+	sum=`expr $1 + $2`
+	echo "Addition = $sum"
+}
+
+multiply()
+{
+	mul=`expr $1 \* $2`
+	echo "Multiply = $mul"
+}
+
+echo enter two numbers
+read no1 no2
+
 while [ true ]
 do
 	echo "****MENU****"
@@ -12,7 +27,9 @@ do
 	read choice
 
 	case $choice in
-		+) addition no1 no2;;
+		+) addition $no1 $no2;;
+
+		x) multiply $no1 $no2;;
 
 		*)echo "Wrong choice! Try again";;
 	esac
