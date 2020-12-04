228769241837fcfd24b432898c1f4a911c0d06ce
Jesus Esquer <jme2791@gmail.com>
2020-12-03 22:00:02 -0700

    Palindrome working!

--- Reverse-Number.sh
+++ Reverse-Number.sh
@@ -5,13 +5,21 @@
 
 echo Enter Number to Reverse
 read number
+num=$number
 reverse=0
 
 while [ $number -gt 0 ]
 do
-	rem=`expr $number %10`
+	rem=`expr $number % 10`
 	reverse=`expr $reverse \* 10 + $rem`
 	number=`expr $number / 10`
 done
 
 echo Reverse=$reverse;
+
+if [ $num -eq $reverse ]
+then
+	echo "Palindrome!!!"
+else
+	echo "Not Palindrome!"
+fi
