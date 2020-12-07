426a3387c9343f40e381c66d386e1b71cc382ed1
Jesus Esquer <jme2791@gmail.com>
2020-12-06 21:26:49 -0700

    while loop added

--- while-loop.sh
+++ while-loop.sh
@@ -2,3 +2,22 @@
 
 # Author : Jesus Esquer
 # Script Name : While Demo
+
+echo "Hi, tell me something"
+read message
+
+while [ true ]
+	do
+		echo "Hi, tell me something"
+		read message
+		case $message in
+
+			hi) echo "Hi!, How are you?"
+
+			bye) echo "Bye! Have a great day!"
+				exit;;
+
+			*) echo "Sorry, wrong command either say hi or bye"
+
+		esac
+	done
